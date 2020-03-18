e8abce10ab977e63241e8abe352edae2756b7669
user <12>
2020-03-19 00:40:38 +0200

    sceleton loadr

---
+++ src/components/SkeletonProduct.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="product-box">
+        <div class="mt-2 image"></div>
+        <div class="mt-2 title"></div>
+        <div class="mt-2 desc"></div>
+        <div class="mt-1 desc"></div>
+        <div class="mt-1 desc"></div>
+        <div class="mt-2 mb-2 price"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SkeletonProduct"
+    }
+</script>
+
+<style scoped>
+    .product-box {
+        padding-left: 10px;
+        padding-right: 10px;
+        text-align: left;
+        border-radius: 10px;
+        float: left;
+    }
+
+    .image {
+        width: 250px;
+        height: 250px;
+    }
+
+    .title {
+        width: 90%;
+        height: 25px;
+    }
+
+    .desc {
+        width: 80%;
+        height: 10px;
+    }
+
+    .price {
+        width: 25%;
+        height: 25px;
+    }
+
+    .product-box div {
+        background-color: #BEBEBE;
+    }
+</style>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="products">
-        <div class="col-md-12" v-if="hasProduct">
-            {{ this.$store.state.products }}
+        <div v-if="hasProduct">
+            <transition name="fade">
+                <div class="row" v-if="this.$store.state.products.length > 0" key= 'product'>
+                    <div class="col-md-3" v-for="product in products" :key="product">
+                        <div class="product-box">
+                            <div class="mt-2" :class="(product.img ? 'image' : 'dark-image')">{{ product.img }}</div>
+                            <div class="mt-2 title">{{ product.name }}</div>
+                            <div class="mt-2 desc">{{ product.desc }}</div>
+                            <div class="mt-2 mb-2 price">{{ (product.cost).toFixed(2) }} ₴</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" v-else key='skeleton'>
+                    <app-skeleton-product class="col-md-3" v-for="c in 4" :key="c"></app-skeleton-product>
+                </div>
+            </transition>
         </div>
         <div v-else>
             <h1>
@@ -12,20 +26,25 @@
 </template>
 
 <script>
+    import SkeletonProduct from '../components/SkeletonProduct'
+
     export default {
         name: "Products",
         data() {
             return {
                 hasProduct: true,
+                products: [],
             }
+        },
+        components: {
+            appSkeletonProduct: SkeletonProduct
         },
         methods: {
             getProducts() {
                 this.$http.get('http://abq.loc/api/users').then((response) => {
                     if (Object.keys(response.data).length !== 0) {
-                        this.$store.state.products = response.data
-                    }
-                    else {
+                        this.products = this.$store.state.products = response.data
+                    } else {
                         this.hasProduct = false
                     }
                 })
@@ -40,4 +59,42 @@
 
 <style scoped>
 
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+
+    .products {
+        margin-top: 30px;
+        padding: 15px;
+    }
+
+    .product-box {
+
+    }
+
+    .image {
+        width: 250px;
+        height: 250px;
+    }
+
+    .dark-image {
+        width: 250px;
+        height: 250px;
+        background-color: rebeccapurple;
+    }
+
+    .title {
+
+    }
+
+    .desc {
+
+    }
+
+    .price {
+
+    }
 </style>
